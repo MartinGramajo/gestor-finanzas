1bd78c6e51304e5a713b2a3e49458720031279b1
¨mago¨ <martingramajo08@gmail.com>
2024-07-01 20:35:25 -0300

    agrego css, banner al home screen

--- src/pages/FormularioAgregarScreen.jsx
+++ src/pages/FormularioAgregarScreen.jsx
@@ -11,11 +11,11 @@ const FormularioAgregarScreen = () => {
         </div>
         <section className="container">
           <div className="row">
+            <h5 className="text-center mb-4">
+              Agrega una Nueva Gestión de Finanzas
+            </h5>
             <article className="text-center col-md-6 col-12 d-flex align-items-center justify-content-center">
               <div>
-                <h5 className="text-center mb-4">
-                  Agrega una Nueva Gestión de Finanzas
-                </h5>
                 <h6>
                   Completa el siguiente formulario para registrar una nueva
                   gestión financiera.
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import { Image, Table, Alert } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import gestion from "../assets/gestorHome.png";
 
 const HomeScreen = () => {
   const [textoBusqueda, setTextoBusqueda] = useState("");
   const listadoTransacciones = useSelector((state) => state.transaction);
-  console.log("HomeScreen ~ listadoTransacciones:", listadoTransacciones);
 
   const handleBusquedaChange = (event) => {
     setTextoBusqueda(event.target.value);
@@ -20,43 +21,64 @@ const HomeScreen = () => {
 
   return (
     <div>
-      <h2>Filtros de Categorías</h2>
-      <div>
-        <label htmlFor="busqueda">Buscar por categoría:</label>
-        <input
-          id="busqueda"
-          type="text"
-          value={textoBusqueda}
-          onChange={handleBusquedaChange}
-          placeholder="Ingrese categoría"
-        />
+      <div className="banner-image-container">
+        <Image className="banner-image" src={gestion} fluid />
       </div>
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Descripcion</th>
-              <th>Monto</th>
-              <th>Categorias</th>
-              <th>Fecha</th>
-              <th>Gasto o Ingreso</th>
-            </tr>
-          </thead>
-          <tbody>
-            {transaccionesFiltradas.map((transaction) => (
-              <tr key={transaction.id}>
-                <td>{transaction.id}</td>
-                <td>{transaction.descripcion}</td>
-                <td>{transaction.monto}</td>
-                <td>{transaction.categorias}</td>
-                <td>{transaction.fecha}</td>
-                <td>{transaction.gastoIngreso}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <section className="container">
+        <div className="text-center mb-4">
+          <h5>Controla tus Finanzas con Facilidad</h5>
+          <h6>
+            Descubre una nueva forma inteligente de administrar tu dinero con
+            nuestra aplicación de gestión de finanzas. Desde presupuestos
+            personalizados hasta seguimiento detallado de gastos e ingresos,
+            nuestra herramienta te proporciona las herramientas necesarias para
+            tomar decisiones financieras informadas y alcanzar tus metas
+            económicas. Simplifica tu vida financiera y empieza a planificar tu
+            futuro con confianza hoy mismo.
+          </h6>
+        </div>
+        <div>
+          <label htmlFor="busqueda">Buscar por categoría:</label>
+          <input
+            id="busqueda"
+            type="text"
+            className="ms-2"
+            value={textoBusqueda}
+            onChange={handleBusquedaChange}
+            placeholder="Ingrese categoría"
+          />
+        </div>
+        <div className="mt-4">
+          {transaccionesFiltradas.length === 0 ? (
+            <Alert variant="info">
+              No se encontraron registros para la búsqueda.
+            </Alert>
+          ) : (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Descripcion</th>
+                  <th>Monto</th>
+                  <th>Categorias</th>
+                  <th>Fecha</th>
+                  <th>Gasto o Ingreso</th>
+                </tr>
+              </thead>
+              <tbody>
+                {transaccionesFiltradas.map((transaction) => (
+                  <tr key={transaction.id}>
+                    <td>{transaction.descripcion}</td>
+                    <td>$ {transaction.monto}</td>
+                    <td>{transaction.categorias}</td>
+                    <td>{transaction.fecha}</td>
+                    <td>{transaction.gastoIngreso}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </div>
+      </section>
     </div>
   );
 };
