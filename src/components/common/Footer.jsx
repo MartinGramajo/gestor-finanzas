1f1780662ef2be3ee0ce2265b3eeca324a7ce81e
¨mago¨ <martingramajo08@gmail.com>
2024-07-01 22:27:00 -0300

    agrego iconos en el footer y en el nav

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,12 @@
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
   return (
     <div className="text-center mt-auto py-4 bg-black text-white">
       <h6 className="fs-12">
-        Rolling Code School © 2024 Todos los derechos reservados
+        <FontAwesomeIcon className="me-2" icon={faCode} /> Rolling Code School ©
+        2024 Todos los derechos reservados
       </h6>
     </div>
   );
--- src/components/common/NavReact.jsx
+++ src/components/common/NavReact.jsx
@@ -1,25 +1,33 @@
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, NavLink } from "react-router-dom";
 
 export default function NavReact() {
+  function cerrar() {
+    let navbar = document.querySelector(".navbar-toggler");
+    navbar.click();
+  }
+
   return (
     <Navbar className="bg-dark" variant="dark" expand="lg" id="principal">
       <Container>
         <Navbar.Brand className="logo-texto" as={Link} to="/">
+          <FontAwesomeIcon className="me-2" icon={faCode} />
           Rolling-code
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">
+            <Nav.Link as={NavLink} to="/" onClick={cerrar}>
               Home
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/agregar">
+            <Nav.Link as={NavLink} to="/agregar" onClick={cerrar}>
               Agregar
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/acciones">
+            <Nav.Link as={NavLink} to="/acciones" onClick={cerrar}>
               Acciones
             </Nav.Link>
           </Nav>
